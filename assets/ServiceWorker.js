a0959b1ca90872a5c86de70d993c4704e4d9d33c
The MAZZTer <megazzt@gmail.com>
2023-11-22 17:48:52 -0500

    v1.1.1

--- assets/ServiceWorker.js
+++ assets/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "MZZT-Dark Forces Showcase-1.1.0";
+const cacheName = "MZZT-Dark Forces Showcase-1.1.1";
 const contentToCache = [
 	"../Build/WebAssembly.loader.js",
 	"../Build/WebAssembly.framework.js",
